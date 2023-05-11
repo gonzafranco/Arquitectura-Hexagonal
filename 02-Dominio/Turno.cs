8ad5f0d47059f27660706b76983f59b93dc21e5b
gonzafranco <gonzafrancoandres@gmail.com>
2023-05-11 20:35:59 -0300

    first commit

---
+++ 02-Dominio/Turno.cs
@@ -0,0 +1,29 @@
+﻿using System.Text.RegularExpressions;
+
+namespace _02_Dominio
+{
+    public class Turno
+    {
+        private string titulo;
+        private string descripcion;
+        private string cuerpo;
+        private DateTime fechaDeCreacion;
+        private string estado;
+
+        public Turno(
+             string titulo,
+             string descripcion,
+             string cuerpo,
+             string estado
+        )
+        {
+            this.titulo = titulo;
+            this.descripcion = descripcion;
+            this.cuerpo = cuerpo;
+            this.fechaDeCreacion = DateTime.Now;
+            this.estado = estado;
+        }
+
+
+    }
+}
