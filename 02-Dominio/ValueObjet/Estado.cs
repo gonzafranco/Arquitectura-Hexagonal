0f5950d1dfd9f2340ee0080f93285a0eb7d5c5a1
PriKrutzinna <prikrutzinna07@gmail.com>
2023-05-16 19:00:18 -0300

    add valueObject

---
+++ 02-Dominio/ValueObjet/Estado.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_Dominio.ValueObjet
+{
+    internal class Estado
+    {
+    }
+}
